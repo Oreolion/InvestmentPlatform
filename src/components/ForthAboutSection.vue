7f1816c184dd87ee5f504ee59f99a40d3f2d2a19
oreolion <remyoreo11@gmail.com>
2024-03-16 05:43:18 -0700

    add: new component to about page;

---
+++ src/components/ForthAboutSection.vue
@@ -0,0 +1,41 @@
+<template>
+  <section>
+    <h1>We have a place for our future generation</h1>
+    <p>
+      Create a trading account for your kids and allow them to leave the life
+      they desire in the future by building the foundation for them now
+    </p>
+  </section>
+</template>
+
+<script setup></script>
+
+<style scoped>
+section {
+  background: url("../assets/pexels-rdne-stock-project-8369687.jpg");
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-attachment: fixed;
+  padding: 5rem 5%;
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+  align-items: center;
+  justify-content: center;
+  min-height: 40rem;
+  color: #e67e22;
+}
+
+h1 {
+  font-size: 5rem;
+  margin-bottom: 2rem;
+  font-weight: bold;
+}
+
+p {
+  font-size: 2.5rem;
+  font-weight: bold;
+  max-width: 85rem;
+}
+</style>
--- src/pages/About.vue
+++ src/pages/About.vue
@@ -4,6 +4,7 @@
         <FirstABoutSection></FirstABoutSection>
         <SecondABoutSection></SecondABoutSection>
         <ThirdABoutSection></ThirdABoutSection>
+        <ForthABoutSection></ForthABoutSection>
         <Footer></Footer>
     </div>
 </template>
@@ -13,6 +14,7 @@ import Navbar from "../components/Navbar.vue";
 import FirstABoutSection from "../components/FirstAboutSection.vue";
 import SecondABoutSection from "../components/SecondAboutSection.vue";
 import ThirdABoutSection from "../components/ThirdAboutSection.vue";
+import ForthABoutSection from "../components/ForthAboutSection.vue";
 import Footer from "../components/Footer.vue";
 
 
