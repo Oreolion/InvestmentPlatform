f4b10cb8b6a8c74f3b54ab13852384047fd670ff
oreolion <remyoreo11@gmail.com>
2024-03-19 04:28:29 -0700

    update error page;

--- src/pages/NotFound.vue
+++ src/pages/NotFound.vue
@@ -1,9 +1,46 @@
 <template>
-  <div style="color: red">
-    <h1>Error, Page not found</h1>
-  </div>
+  <section>
+    <div class="box">
+      <div style="color: red">
+        <h1>SOMETHING WENT WRONG!! <br />You Seem Lost...</h1>
+      </div>
+      <button>
+        <router-link to="/">&lt;&lt;&lt; Go Back Home</router-link>
+      </button>
+    </div>
+    <div class="box">
+      <img src="../assets/flame-error-404.png" alt="" />
+    </div>
+  </section>
 </template>
 
 <script setup lang="ts"></script>
 
-<style scoped></style>
+<style scoped>
+section {
+  min-height: 40rem;
+  padding: 15rem 10%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-family: "Trebuchet MS", "Lucida Sans Unicode", "Lucida Grande",
+    "Lucida Sans", Arial, sans-serif;
+  gap: 5rem;
+}
+
+.box div {
+  margin-bottom: 4rem;
+}
+
+h1 {
+  font-size: 4rem;
+  font-weight: bold;
+}
+button {
+  font-size: 2rem;
+  font-weight: bold;
+  width: 23rem;
+  height: 7rem;
+  white-space: nowrap;
+}
+</style>
